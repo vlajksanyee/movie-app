b79be59cae18c0a3c3c52762c1838a16cb46cd69
Sándor Vlajk <71777468+vlajksanyee@users.noreply.github.com>
2025-04-08 13:00:22 +0200

    new background and new padding
    * feat: new purple background
    
    * feat: new padding

--- movie-app/ContentView.swift
+++ movie-app/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+        .padding(.horizontal, 10)
+        .background(.purple)
     }
 }
 
