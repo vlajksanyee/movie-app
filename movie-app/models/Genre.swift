3ce63fc3bc15175d27ff8c95d59ae388085ccf71
Sándor Vlajk <vlajksanyi@gmail.com>
2025-04-08 15:54:58 +0200

    feat: implement genre section

---
+++ movie-app/models/Genre.swift
@@ -0,0 +1,11 @@
+//
+//  Genre.swift
+//  movie-app
+//
+//  Created by Sandor Vlajk on 2025. 04. 08..
+//
+
+struct Genre: Identifiable, Hashable, Equatable {
+    let id: Int
+    let name: String
+}
