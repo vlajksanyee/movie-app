5385288ad03b66421399468757bec69b1334c7a5
Sándor Vlajk <71777468+vlajksanyee@users.noreply.github.com>
2025-04-08 15:58:55 +0200

    feat: implement genre section

---
+++ movie-app/ui/genre-section/GenreSectionView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  movie-app
+//
+//  Created by Sandor Vlajk on 2025. 04. 08..
+//
+
+import SwiftUI
+
+class GenreSectionViewModel: ObservableObject {
+    @Published var genres: [Genre] = []
+    
+    func loadGenres() {
+        self.genres = [
+            Genre(id: 1, name: "Adventure"),
+            Genre(id: 2, name: "Sci-fi"),
+            Genre(id: 3, name: "Fantasy"),
+            Genre(id: 4, name: "Comedy")
+        ]
+    }
+}
+
+struct GenreSectionView: View {
+    
+    @StateObject private var viewModel = GenreSectionViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.genres) { genre in
+                ZStack {
+                    NavigationLink(destination: Color.gray) {
+                        EmptyView()
+                    }
+                    .opacity(0)
+                    
+                    HStack {
+                        Text(genre.name)
+                            .font(Fonts.title)
+                            .foregroundStyle(.primary)
+                        Spacer()
+                        Image(.rightArrow)
+                    }
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .navigationTitle("genreSection.title")
+        }
+        .onAppear {
+            viewModel.loadGenres()
+        }
+    }
+    
+}
+
+#Preview {
+    GenreSectionView()
+}
