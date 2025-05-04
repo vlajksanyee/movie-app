f2e089b6cd63ee0dd93039510f003d007b561697
Sándor Vlajk <vlajksanyi@gmail.com>
2025-05-04 06:41:00 +0200

    feat: add view and viewmodel for favorites

--- movie-app/ui/favorites/FavoritesView.swift
+++ movie-app/ui/favorites/FavoritesView.swift
@@ -13,8 +13,34 @@ struct FavoritesView: View {
     
     var body: some View {
         NavigationView {
-            Text("Favorites Screen")
+            if viewModel.movies.isEmpty {
+                VStack {
+                    Spacer()
+                    Text("favorites.empty")
+                        .multilineTextAlignment(.center)
+                        .font(Fonts.emptyStateText)
+                        .foregroundColor(.invertedMain)
+                        .navigationTitle("favorites.title")
+                    Spacer()
+                }
+            } else {
+                ScrollView {
+                    LazyVStack(spacing: LayoutConst.normalPadding) {
+                        ForEach(viewModel.movies) { movie in
+                            MovieCell(movie: movie)
+                                .frame(height: 277)
+                        }
+                    }
+                    .padding(.horizontal, LayoutConst.normalPadding)
+                    .padding(.top, LayoutConst.normalPadding)
+                }
                 .navigationTitle("favorites.title")
+            }
+        }
+        .onAppear {
+            Task {
+                await viewModel.favoriteMovies()
+            }
         }
     }
 }
--- movie-app/ui/favorites/FavoritesViewModel.swift
+++ movie-app/ui/favorites/FavoritesViewModel.swift
@@ -6,11 +6,29 @@
 //
 
 import Foundation
+import Combine
+import InjectPropertyWrapper
 
 protocol FavoritesViewModelProtocol: ObservableObject {
-    // TODO: Add favorite movie related properties and methods
+    var movies: [Movie] { get set }
+    func favoriteMovies() async
 }
 
 class FavoritesViewModel: FavoritesViewModelProtocol {
-    // TODO: Implement favorite movies functionality
+    @Published var movies: [Movie] = []
+    
+    @Inject
+    private var service: MoviesServiceProtocol
+        
+    func favoriteMovies() async {
+        do {
+            let request = FavoriteMoviesRequest(account_id: "21958080")
+            let movies = try await service.favoriteMovies(req: request)
+            DispatchQueue.main.async {
+                self.movies = movies
+            }
+        } catch {
+            print("Error loading favorites: \(error)")
+        }
+    }
 }
