7adbb891016b11b40dd852250e117a9e58803b66
Sándor Vlajk <vlajksanyi@gmail.com>
2025-05-28 07:39:04 +0200

    chore: update localization

--- movie-app/helpers/ErrorPresentable.swift
+++ movie-app/helpers/ErrorPresentable.swift
@@ -15,43 +15,43 @@ extension ErrorPresentable {
     func toAlertModel(_ error: Error) -> AlertModel? {
         guard let error = error as? MovieError else {
             return AlertModel(
-                title: NSLocalizedString("unexpectederror.title", comment: ""),
-                message: NSLocalizedString("unexpectederror.message", comment: ""),
-                dismissButtonTitle: NSLocalizedString("dismissbutton.title", comment: "")
+                title: NSLocalizedString("unexpectederror.title".localized(), comment: ""),
+                message: NSLocalizedString("unexpectederror.message".localized(), comment: ""),
+                dismissButtonTitle: NSLocalizedString("dismissbutton.title".localized(), comment: "")
             )
         }
         switch error {
         case .invalidApiKeyError(let message):
             return AlertModel(
-                title: NSLocalizedString("apierror.message", comment: ""),
+                title: NSLocalizedString("apierror.message".localized(), comment: ""),
                 message: message,
-                dismissButtonTitle: NSLocalizedString("dismissbutton.title", comment: "")
+                dismissButtonTitle: NSLocalizedString("dismissbutton.title".localized(), comment: "")
             )
         case .clientError:
             return AlertModel(
-                title: NSLocalizedString("clienterror.title", comment: ""),
+                title: NSLocalizedString("clienterror.title".localized(), comment: ""),
                 message: error.localizedDescription,
-                dismissButtonTitle: NSLocalizedString("dismissbutton.title", comment: "")
+                dismissButtonTitle: NSLocalizedString("dismissbutton.title".localized(), comment: "")
             )
         case .mappingError:
             return AlertModel(
-                title: NSLocalizedString("mappingerror.title", comment: ""),
-                message: NSLocalizedString("mappingerror.message", comment: ""),
-                dismissButtonTitle: NSLocalizedString("dismissbutton.title", comment: "")
+                title: NSLocalizedString("mappingerror.title".localized(), comment: ""),
+                message: NSLocalizedString("mappingerror.message".localized(), comment: ""),
+                dismissButtonTitle: NSLocalizedString("dismissbutton.title".localized(), comment: "")
             )
         case .noInternetError:
             return nil
         case .serverError:
             return AlertModel(
-                title: NSLocalizedString("servererror.title", comment: ""),
+                title: NSLocalizedString("servererror.title".localized(), comment: ""),
                 message: error.localizedDescription,
-                dismissButtonTitle: NSLocalizedString("dismissbutton.title", comment: "")
+                dismissButtonTitle: NSLocalizedString("dismissbutton.title".localized(), comment: "")
             )
         default:
             return AlertModel(
-                title: NSLocalizedString("unexpectederror.title", comment: ""),
-                message: NSLocalizedString("unexpectederror.message", comment: ""),
-                dismissButtonTitle: NSLocalizedString("dismissbutton.title", comment: "")
+                title: NSLocalizedString("unexpectederror.title".localized(), comment: ""),
+                message: NSLocalizedString("unexpectederror.message".localized(), comment: ""),
+                dismissButtonTitle: NSLocalizedString("dismissbutton.title".localized(), comment: "")
             )
         }
     }
